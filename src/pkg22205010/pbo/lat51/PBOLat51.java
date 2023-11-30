5a11f6957a471e5f3a09ae9d53bcc48c926c0ca2
isnaen20 <149252578+isnaen20@users.noreply.github.com>
2023-11-30 21:42:19 +0700

    Add files via upload

---
+++ src/pkg22205010/pbo/lat51/PBOLat51.java
@@ -0,0 +1,86 @@
+Dosen dosen = new Dosen("41227829930", "Rizki Adam Kurniawan", 27, "PBO");
+        Mahasiswa mahasiswa = new Mahasiswa("10110269", "Nindi", 17, "PBO2");
+
+        // Menampilkan informasi tentang Dosen
+        System.out.println("NIP DOSEN: " + dosen.getNip());
+        System.out.println("Saya Dosen");
+        System.out.println("Saya " + dosen.getNama() + " umur " + dosen.getUmur() + " tahun sedang mengajar matakuliah " + dosen.getMataKuliah());
+        System.out.println();
+
+        // Menampilkan informasi tentang Mahasiswa
+        System.out.println("NAMA MAHASISWA: " + mahasiswa.getNim());
+        System.out.println("Saya Mahasiswa");
+        System.out.println("Saya " + mahasiswa.getNama() + " umur " + mahasiswa.getUmur() + " tahun sedang belajar di kelas " + mahasiswa.getKelas());
+    }
+}
+
+class Dosen {
+    private String nip;
+    private String nama;
+    private int umur;
+    private String mataKuliah;
+
+    // Konstruktor
+    public Dosen(String nip, String nama, int umur, String mataKuliah) {
+        this.nip = nip;
+        this.nama = nama;
+        this.umur = umur;
+        this.mataKuliah = mataKuliah;
+    }
+
+    // Metode untuk mendapatkan NIP Dosen
+    public String getNip() {
+        return nip;
+    }
+
+    // Metode untuk mendapatkan nama Dosen
+    public String getNama() {
+        return nama;
+    }
+
+    // Metode untuk mendapatkan umur Dosen
+    public int getUmur() {
+        return umur;
+    }
+
+    // Metode untuk mendapatkan mata kuliah yang diajar Dosen
+    public String getMataKuliah() {
+        return mataKuliah;
+    }
+}
+
+class Mahasiswa {
+    private String nim;
+    private String nama;
+    private int umur;
+    private String kelas;
+
+    // Konstruktor
+    public Mahasiswa(String nim, String nama, int umur, String kelas) {
+        this.nim = nim;
+        this.nama = nama;
+        this.umur = umur;
+        this.kelas = kelas;
+    }
+
+    // Metode untuk mendapatkan NIM Mahasiswa
+    public String getNim() {
+        return nim;
+    }
+
+    // Metode untuk mendapatkan nama Mahasiswa
+    public String getNama() {
+        return nama;
+    }
+
+    // Metode untuk mendapatkan umur Mahasiswa
+    public int getUmur() {
+        return umur;
+    }
+
+    // Metode untuk mendapatkan kelas Mahasiswa
+    public String getKelas() {
+        return kelas;
+    
+    }
+}
